7c4bfdb10f493c4c6065ac9599a1da2c249a6b80
kadetXx <techkadet@gmail.com>
2023-09-04 06:03:37 +0100

    initial commit ⚡️

--- interactions/classes/dom.ts
+++ interactions/classes/dom.ts
@@ -1,10 +1,12 @@
+import { EventEmitter } from "events";
 import { getElements } from "../utils";
 
-export class Dom {
+export class Dom extends EventEmitter {
   element: HTMLElement;
   children: ChildElements;
 
   constructor({ selector, childSelectors }: SelectorProps) {
+    super();
     const { element, children } = getElements({ selector, childSelectors });
 
     this.element = element;
